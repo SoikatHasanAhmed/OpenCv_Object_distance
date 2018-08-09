8cd88ed1d8dcb82db42990f3de69b6086b742eaf
soikat <soikathasan15@gmail.com>
2018-08-09 22:53:57 +0600

    init commit

---
+++ methods.py
@@ -0,0 +1,21 @@
+import  cv2
+import numpy as np
+
+def find_marker(image):
+    # convert the image to grayscale and blur to detect edges
+
+    blurred_frame = cv2.GaussianBlur(image, (5, 5), 0)
+    hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+
+    lower_blue = np.array([38, 86, 0])
+    upper_blue = np.array([121, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    con = max(contours, key=cv2.contourArea)
+
+    return cv2.minAreaRect(con)
+
+def distance_to_camera(knownWidth, focalLength, perWidth):
+    # compute and return the distance from the image to the camera
+    return (knownWidth * focalLength) / perWidth
---
+++ object_distance.py
@@ -0,0 +1,54 @@
+import time
+import urllib.request
+from methods import *
+
+
+#here we put the object 30 cm from the camera to calibraing
+KNOWN_DISTANCE = 30.0
+
+#the width of the object  in cm
+KNOWN_WIDTH = 3.0
+#we use a ip webcam for capture video
+
+url = 'http://192.168.0.4:8080/shot.jpg'
+print("<<<---- taking calibrating Image ---->>>")
+time.sleep(2)
+
+imgResp = urllib.request.urlopen(url)
+imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+c_image = cv2.imdecode(imgNp, -1)
+
+
+
+marker = find_marker(c_image)
+focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+
+
+time.sleep(2)
+
+print("<<<---- Main program Staring ---->>>")
+
+while True:
+
+    imgResp = urllib.request.urlopen(url)
+    imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+    image = cv2.imdecode(imgNp, -1)
+
+
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    marker = find_marker(image)
+    CM = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+
+    #print the output
+    cv2.putText(image, "%.2fcm" % CM,
+                (image.shape[1] - 350, image.shape[0] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                2.0, (255, 0, 0), 3)
+    cv2.imshow("image", image)
+    key = cv2.waitKey(1)
+    #Press Esc to stop the program
+    if key == 27:
+        break
+
+
+cv2.destroyAllWindows()
